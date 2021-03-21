5be9d60df377ebcb1d81cd7b61454ec971801dca
Nagy Veronika <66723821+nveronica@users.noreply.github.com>
2021-03-21 06:04:56 +0100

    Update Controller.java

--- src/main/java/hu/carinquiry/controller/Controller.java
+++ src/main/java/hu/carinquiry/controller/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class HomeController {
 
-	@GetMapping
+	@GetMapping("")
 	public String home() {
 		return "home";
 	}
