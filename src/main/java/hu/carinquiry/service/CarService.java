96e8cadd4061ad882c46a1394c5ac54d148522f1
Your Name <you@example.com>
2021-05-08 18:21:12 +0200

    edit_home.html_Make_to_make

--- src/main/java/hu/carinquiry/service/CarService.java
+++ src/main/java/hu/carinquiry/service/CarService.java
@@ -1,12 +1,8 @@
 package hu.carinquiry.service;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
-
 import hu.carinquiry.dao.MakeRepository;
-import hu.carinquiry.model.Car;
 import hu.carinquiry.model.Make;
 
 @Service
@@ -16,7 +12,6 @@ public class CarService {
 	private MakeRepository makeRepository;
 
 	public Iterable<Make> findCarByMaked() {
-		// TODO Auto-generated method stub
 		return makeRepository.findAll();
 	}
 
