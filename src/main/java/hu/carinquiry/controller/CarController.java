8252c117cdfb3ec45d353bd88b789d7f89804b76
Your Name <you@example.com>
2021-09-21 17:27:04 +0200

    login

--- src/main/java/hu/carinquiry/controller/CarController.java
+++ src/main/java/hu/carinquiry/controller/CarController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import hu.carinquiry.service.CarService;
 
@@ -12,10 +14,18 @@ public class CarController {
 	@Autowired
 	private CarService service;
 
-	@GetMapping("")
+	@GetMapping("makes")
 	public String home(Model model) {
 		
 		model.addAttribute("make", service.findCarByMaked());
+		return "home";
+	}
+	@GetMapping("")
+	public String welcomePage() {
+		return "/index";
+	}
+	@RequestMapping("login")
+	public String login() {
 		return "home";
 	}
 }
---
+++ src/main/java/hu/carinquiry/security/CarInquiryAuthentication.java
@@ -0,0 +1,5 @@
+package hu.carinquiry.security;
+
+public class CarInquiryAuthentication {
+
+}
