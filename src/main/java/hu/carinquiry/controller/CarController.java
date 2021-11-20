afda117b75eaa58ac275483329e5b67236f4223c
Your Name <you@example.com>
2021-11-20 08:33:56 +0100

    h2 db

--- src/main/java/hu/carinquiry/controller/CarController.java
+++ src/main/java/hu/carinquiry/controller/CarController.java
@@ -1,5 +1,6 @@
 package hu.carinquiry.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
